14ff28625d5eba5d0f23cb96e63af19a4fd5cd87
vagrant <vagrant@homestead>
2018-04-05 03:07:05 +0000

    add field and sluggable to ServiceModels

--- app/Models/Services/Services.php
+++ app/Models/Services/Services.php
@@ -9,8 +9,9 @@ use Illuminate\Notifications\Notifiable;
 class Services extends Model
 {
     //
-    use Notifiable,
-        SoftDeletes;
+    use Notifiable;
+    use Sluggable;
+    use SoftDeletes;
 
     const UNACCEPT = 0;
     const ACCEPTED = 1;
@@ -21,7 +22,7 @@ class Services extends Model
     protected $table = 'services';
     protected $primaryKey = 'id';
 
-    protected $fillable = ['service_name', 'status', 'image_url'];
+    protected $fillable = ['service_name', 'status', 'image_url', 'icon_url', 'day_detail', 'amount_detail'];
 
     public static $rules = [
         'service_name' => 'required',
@@ -37,5 +38,20 @@ class Services extends Model
     public function scopePagination($query, $itemPerPages, $currentPage)
     {
         return $query->take($itemPerPages)->skip($itemPerPages * ($currentPage - 1));
+    }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     * @author tantan
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'service_name'
+            ]
+        ];
     }
 }
