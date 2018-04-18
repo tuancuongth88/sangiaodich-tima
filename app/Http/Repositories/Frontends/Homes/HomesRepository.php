391523f8344a0535279bcd5d14f8c5237e6bfd38
tuan cuong <cuongnt@hocmai.vn>
2018-04-18 23:55:53 +0700

    fix giao dien

--- app/Http/Repositories/Frontends/Homes/HomesRepository.php
+++ app/Http/Repositories/Frontends/Homes/HomesRepository.php
@@ -38,7 +38,7 @@ class HomesRepository extends Repository
         $total_reg_loan = User::where('type', NGUOICHOVAY)->count();
 
         $total_bill_day = TransactionHistory::where('status','<>' ,TransactionHistory::STATUS_WAIT_APPROVE)
-                                            ->where('Date(created_at)', DB::raw('CURDATE()'))
+                                            ->whereRaw('Date(created_at)', DB::raw('CURDATE()'))
                                             ->count();
         $total_bill_system = TransactionHistory::count();
         return view('home', ['data' => $data,
